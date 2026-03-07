50f2f262b263c9a06d1ad70a2530700b12afa41b
Shonaike Temitayo <temitayotijani02@gmail.com>
2026-03-06 21:49:08 -0800

    fix cloudinary config

--- server/config/multer.js
+++ server/config/multer.js
@@ -3,9 +3,9 @@ const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-    cloud_name: process.env.dbqd6mpgj,
-    api_key: process.env.485914339994888,
-    api_secret: process.env.eevWcBgz3vOgAlm6yw6TEab08kI
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 const storage = new CloudinaryStorage({

ee00faea4b16bbff8f2ebd529ed157223b566206
Shonaike Temitayo <temitayotijani02@gmail.com>
2026-03-18 09:14:29 -0700

    remove cloudnary

--- server/controllers/printController.js
+++ server/controllers/printController.js
@@ -8,11 +8,11 @@ const fs = require('fs');
 const path = require('path');
 const tmp = require('tmp')
 
-cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-});
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//     api_key: process.env.CLOUDINARY_API_KEY,
+//     api_secret: process.env.CLOUDINARY_API_SECRET
+// });
 
 const getLuluToken = async () => {
     const response = await axios.post(

559d5d7a5e35a0e514c1f7793047116d2419b00a
Shonaike Temitayo <temitayotijani02@gmail.com>
2026-04-22 16:55:02 -0700

    added width and height to coverHtml

--- server/controllers/printController.js
+++ server/controllers/printController.js
@@ -406,7 +406,7 @@ console.log('Cover dimensions in inches:', coverWidth, coverHeight);
 
 
 console.log('Generating cover PDF...');
-const coverHTML = generateCoverHTML(scrapbook, coverStyle, customCoverUrl, null, null, bookSize);
+const coverHTML = generateCoverHTML(scrapbook, coverStyle, customCoverUrl, coverHeight, coverWidth);
 const coverPdfUrl = await generatePDFWithAPI2PDF(coverHTML, bookSize, coverWidth, coverHeight);
 console.log('Cover PDF URL:', coverPdfUrl);
 
